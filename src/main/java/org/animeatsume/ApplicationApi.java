c2d4d6ed799750b08df6212d777a2d4cc10cd377
D-Pow <djp460@nyu.edu>
2020-04-27 17:13:13 -0400

    Add base code for extracting client IP address from request

--- src/main/java/org/animeatsume/ApplicationApi.java
+++ src/main/java/org/animeatsume/ApplicationApi.java
@@ -1,17 +1,23 @@
 package org.animeatsume;
 
 import org.animeatsume.api.controller.NovelPlanetController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApplicationApi {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationApi.class);
+
     @Autowired
     NovelPlanetController novelPlanetController;
 
-//    @CrossOrigin
-//    @PostMapping("/novelPlanet")
-//    public void getNovelPlanetMp4Urls(@RequestBody String novelPlanetUrl, final HttpServletRequest request) {
-        // find out how to get request IP address here
-//    }
+    @CrossOrigin
+    @PostMapping("/novelPlanet")
+    public void getNovelPlanetMp4Urls(ServerHttpRequest request) {
+        log.info("Address is {}", request.getRemoteAddress().getAddress().toString());
+        log.info("Port is {}", String.valueOf(request.getRemoteAddress().getPort()));
+    }
 }
