1991900d7ea8bba71561702e2121809abd81240e
D-Pow <djp460@nyu.edu>
2020-06-14 22:15:50 -0400

    Add ability to save .mp4 files from URLs into the filesystem and notes about usage/next steps
    If the file is saved locally, the seeking-not-working issue from the UrlResource proxy method is resolved since the system handles 'Ranges' header appropriately and loads super fast. However, as it is written currently, the file has to be downloaded before being served to the user, which is a bad experience. Thus, while having the .mp4 files locally is the best option, it doesn't result in the quickest experience. Next step: either use a streaming response or toggle between UrlResource proxy and using downloaded files

--- src/main/java/org/animeatsume/api/controller/NovelPlanetController.java
+++ src/main/java/org/animeatsume/api/controller/NovelPlanetController.java
@@ -10,8 +10,10 @@ import org.springframework.core.io.*;
 import org.springframework.http.*;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.*;
 import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -116,7 +118,211 @@ public class NovelPlanetController {
         }
     }
 
+    /*
+     * TODO Find a way to send video response immediately even if not fully loaded.
+     *  Options:
+     *  1. If file doesn't exist, spawn thread to save the file, and return UrlResource. Subsequent requests will
+     *     return the file. Hopefully, even if the file isn't finished downloading, what has finished downloading can
+     *     still be returned. (This is just a theory and needs validating.)
+     *  2. Return a stream and use "Range" header parsing: Even with the "Range" header, if the range is "bytes=X-"
+     *     without an ending, a large chunk of file will still need to be loaded and returned. A stream could
+     *     allow for streaming parts of the file as it's loaded instead of waiting for the whole thing to be
+     *     loaded before sending a response.
+     *
+     * For the byte-range header:
+     *  - https://stackoverflow.com/questions/28427339/how-to-implement-http-byte-range-requests-in-spring-mvc
+     *
+     * For streams:
+     *  Some tutorial suggestions:
+     *  - https://melgenek.github.io/spring-video-service
+     *  - https://www.baeldung.com/spring-resttemplate-download-large-file
+     *  - https://github.com/ItamarBenjamin/stream-rest-template/blob/master/src/main/java/com/ibinyami/spring/resttemplate/StreamRestTemplate.java
+     *  - https://docs.spring.io/spring/docs/3.2.x/spring-framework-reference/html/resources.html
+     *  Other tutorials, but these use StreamingResponseBody, which is spring-web, not spring-webflux
+     *  - https://dzone.com/articles/streaming-data-with-spring-boot-restful-web-servic
+     *  - https://stackoverflow.com/questions/47277640/how-to-proxy-a-http-video-stream-to-any-amount-of-clients-through-a-spring-webse
+     *  Some solutions for (non-)reactive implementations
+     *  Non-reactive solution:
+     *  - https://stackoverflow.com/questions/20333394/return-a-stream-with-spring-mvcs-responseentity
+     *    (not helpful b/c uses InputStreamResource which throws "Cannot convert an InputStreamResource to a ResourceRegion")
+     *  - https://stackoverflow.com/questions/15951439/resteasy-client-3-0-handling-large-content
+     *  - https://stackoverflow.com/questions/32812645/spring-boot-writing-media-image-mp3-mp4-file-to-response-output-stream
+     *  Reactive solution:
+     *  - Requires using WebFlux client instead of RestTemplate
+     *    - WebFlux tutorial: https://spring.io/guides/gs/async-method/
+     *    - Overview: https://docs.spring.io/spring/docs/current/spring-framework-reference/web-reactive.html#webflux-client
+     *  - Consider a multi-part response
+     *    - MultipartBodyBuilder (non-reactive)
+     *    - Multipart (reactive): https://docs.spring.io/spring/docs/current/spring-framework-reference/integration.html#rest-template-multipart
+     */
     public ResponseEntity<Resource> getVideoSrcStreamFromMp4Url(String url, String rangeHeader) {
         return Requests.getUrlResourceStreamResponse(url);
+//        return getVideoSrcStreamFromMp4UrlFile(url, rangeHeader);
     }
+
+    public ResponseEntity<Resource> getVideoSrcStreamFromMp4UrlFile(String url, String rangeHeader) {
+        String baseDirectoryName = ".";
+        String videoDirectoryName = "videos";
+        String showDirectoryName = "Your Lie In April";
+        String showDirectoryPath = baseDirectoryName + File.separator + videoDirectoryName + File.separator + showDirectoryName;
+        String fileName = "Episode-1.mp4";
+        String filePath = showDirectoryPath + File.separator + fileName;
+        File showDirectory = new File(showDirectoryPath);
+        File videoFileOnDisk = new File(filePath);
+
+        if (!showDirectory.exists()) {
+            boolean success = showDirectory.mkdirs();
+
+            if (success) {
+                log.info("New directory for show ({}) was created at path ({})",
+                    showDirectoryName,
+                    showDirectory.getAbsolutePath()
+                );
+            } else {
+                log.error("Failed to make directory for show ({}). Current process' working directory = ({})",
+                    showDirectoryName,
+                    new File(baseDirectoryName).getAbsolutePath()
+                );
+
+                return ResponseEntity.noContent().build();
+            }
+        }
+
+        if (!videoFileOnDisk.exists()) {
+            boolean success = saveNewVideoFromUrl(url, rangeHeader, videoFileOnDisk);
+
+            if (!success) {
+                return ResponseEntity.noContent().build();
+            }
+        }
+
+        FileSystemResource resourceForResponseBody = new FileSystemResource(videoFileOnDisk);
+
+        return ResponseEntity
+            .status(HttpStatus.PARTIAL_CONTENT)
+            .contentType(
+                MediaTypeFactory
+                    .getMediaType(resourceForResponseBody)
+                    .orElse(MediaType.APPLICATION_OCTET_STREAM) // alt: MediaType.asMediaType(MimeType.valueOf("video/mp4"))
+            )
+            .body(resourceForResponseBody);
+    }
+
+    public boolean saveNewVideoFromUrl(String url, String rangeHeader, File videoFileOnDisk) {
+        RestTemplate mp4Request = new RestTemplate();
+        Requests.addAcceptableMediaTypes(mp4Request, MediaType.parseMediaType("video/mp4"));
+
+        Boolean success = mp4Request.execute(
+            url,
+            HttpMethod.GET,
+            clientHttpRequest -> {
+                HttpHeaders headers = clientHttpRequest.getHeaders();
+
+                if (rangeHeader != null && !rangeHeader.isEmpty()) {
+                    headers.set("Range", rangeHeader);
+                }
+            },
+            clientHttpResponse -> {
+                log.info(
+                    "Response callback: status({}), headers({})",
+                    clientHttpResponse.getStatusCode(),
+                    clientHttpResponse.getHeaders()
+                );
+
+                try (
+                    InputStream responseBodyInputStream = clientHttpResponse.getBody();
+                    FileOutputStream fileOutputStream = new FileOutputStream(videoFileOnDisk)
+                ) {
+                    StreamUtils.copy(responseBodyInputStream, fileOutputStream);
+                } catch (IOException e) {
+                    log.error("Failed to download video from URL({}) to file ({}). Error:", url, videoFileOnDisk.getAbsolutePath());
+                    e.printStackTrace();
+
+                    return false;
+                }
+
+                log.info(
+                    "Response callback: video from ({}} downloaded to ({})",
+                    url,
+                    videoFileOnDisk.getAbsolutePath()
+                );
+
+                return true;
+            }
+        );
+
+        return success != null && success;
+    }
+
+    // Unsuccessful attempt from non-reactive solution from bullet-points above
+    // at URL: https://stackoverflow.com/questions/15951439/resteasy-client-3-0-handling-large-content
+//    public ResponseEntity<Resource> getVideoSrcStreamFromMp4UrlStreamingHttp(String url, String rangeHeader) throws IOException {
+//        log.info("Getting MP4 sream with range header=[{}] for url=[{}]", rangeHeader, url);
+//
+//        UrlResource mp4UrlResource = new UrlResource(url);
+//        InputStream mp4InputStream = mp4UrlResource.getInputStream();
+//        HttpHeaders mp4Headers = new RestTemplate().headForHeaders(url);
+//        List<String> contentLengthHeader = mp4Headers.get("Content-Length");
+//
+//        log.info("Mime type = {}", MediaTypeFactory.getMediaType(mp4UrlResource));
+//
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.setContentType(
+//            MediaTypeFactory
+//                .getMediaType(mp4UrlResource)
+//                .orElse(MediaType.valueOf(
+//                    MediaType.APPLICATION_OCTET_STREAM_VALUE
+//                ))
+//        );
+//
+//        RestTemplate mp4Request = new RestTemplate();
+//        Requests.addAcceptableMediaTypes(mp4Request, MediaType.parseMediaType("video/mp4"));
+//
+//        log.info("Before execute");
+//        ClientHttpResponse mp4Response = mp4Request.execute(
+//            url,
+//            HttpMethod.GET,
+//            clientHttpRequest -> {
+//                HttpHeaders headers = clientHttpRequest.getHeaders();
+//                headers.set("Range", rangeHeader);
+//
+//                try (OutputStream requestOutputStream = clientHttpRequest.getBody()) {
+//                    StreamUtils.copy(mp4InputStream, requestOutputStream);
+//                }
+//            },
+//            clientHttpResponse -> clientHttpResponse
+//        );
+//
+//        log.info("After execute, before StreamingOutput");
+//
+//        StreamingHttpOutputMessage streamingResponseBody = new StreamingHttpOutputMessage() {
+//            Body body = outputStream -> StreamUtils.copy(mp4InputStream, outputStream);
+//
+//            @Override
+//            public void setBody(Body body) {
+//                this.body = body;
+//            }
+//
+//            @Override
+//            public OutputStream getBody() throws IOException {
+//                return null;
+//            }
+//
+//            @Override
+//            public HttpHeaders getHeaders() {
+//                return responseHeaders;
+//            }
+//        };
+//
+//        log.info("Trying to return ResponseEntity");
+//
+//        try {
+//            return new ResponseEntity(streamingResponseBody, responseHeaders, HttpStatus.PARTIAL_CONTENT);
+//        } catch(Exception e) {
+//            log.error("Error returning ResponseEntity<StreamingHttpOutputMessage>:");
+//            e.printStackTrace();
+//        }
+//
+//        return ResponseEntity.noContent().build();
+//    }
 }
