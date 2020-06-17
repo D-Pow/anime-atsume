5f5d49adbe1b238230fc8049c15aa55deec62383
D-Pow <djp460@nyu.edu>
2020-06-17 19:22:40 -0400

    Minor: Allow KissanimeSearchResponse to be instantiated with all or no args

--- src/main/java/org/animeatsume/api/model/KissanimeSearchResponse.java
+++ src/main/java/org/animeatsume/api/model/KissanimeSearchResponse.java
@@ -2,10 +2,13 @@ package org.animeatsume.api.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class KissanimeSearchResponse {
     @Data
     @AllArgsConstructor
