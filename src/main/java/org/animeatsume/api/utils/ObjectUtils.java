aa4ac7c1c202120f4d20c211a9b63ffd38dce91e
D-Pow <djp460@nyu.edu>
2020-06-18 13:10:41 -0400

    Add new ObjectUtils file, including functions for simplifying getting all CompletedFuture results

---
+++ src/main/java/org/animeatsume/api/utils/ObjectUtils.java
@@ -0,0 +1,45 @@
+package org.animeatsume.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class ObjectUtils {
+    private static final Logger log = LoggerFactory.getLogger(ObjectUtils.class);
+
+    public static <T> List<T> getAllCompletableFutureResults(
+        List<CompletableFuture<T>> futures,
+        Consumer<T> sideEffect
+    ) {
+        return getAllCompletableFutureResults(futures, (result, index) -> sideEffect.accept(result));
+    }
+
+    public static <T> List<T> getAllCompletableFutureResults(
+        List<CompletableFuture<T>> futures,
+        BiConsumer<T, Integer> sideEffect
+    ) {
+        List<T> results = new ArrayList<>();
+
+        for (int i = 0; i < futures.size(); i++) {
+            CompletableFuture<T> future = futures.get(i);
+            T result = null;
+
+            try {
+                result = future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                log.error("Could not get future. Cause = {}, Message = {}", e.getCause(), e.getMessage());
+            }
+
+            sideEffect.accept(result, i);
+            results.add(result);
+        }
+
+        return results;
+    }
+}
