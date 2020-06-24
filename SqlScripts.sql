fa80d5d3ce42d6fcfa4a1202da645d7798b00460
D-Pow <djp460@nyu.edu>
2020-06-24 14:37:08 -0400

    Refactor database to use image hash instead of filename IDs
    Since file names can change over time, store hashes instead of file names

--- SqlScripts.sql
+++ SqlScripts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS captcha_answers (
     prompt VARCHAR(32) NOT NULL,
-    image_id VARCHAR(200) NOT NULL,
-    PRIMARY KEY (prompt, image_id)
+    image_hash VARCHAR(200) NOT NULL,
+    PRIMARY KEY (prompt, image_hash)
 );
