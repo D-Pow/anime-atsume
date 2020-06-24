ef86eb8fe182f1128ff5eebe596ddafd09dfd811
D-Pow <djp460@nyu.edu>
2020-06-24 15:27:44 -0400

    Bug fix: Remove quotes from LIKE statement
    Since JPA queries already use prepared statements, there is no need to wrap in quotes. Further, it breaks the normal prepared-statement injection and reads it as a literal string

--- src/main/java/org/animeatsume/dao/persistence/CaptchaAnswerRepository.java
+++ src/main/java/org/animeatsume/dao/persistence/CaptchaAnswerRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CaptchaAnswerRepository extends JpaRepository<CaptchaAnswer, CaptchaAnswer.CaptchaAnswerPk> {
-    @Query("SELECT captcha FROM CaptchaAnswer captcha WHERE captcha.prompt LIKE '%:prompt%'")
+    @Query("SELECT captcha FROM CaptchaAnswer captcha WHERE captcha.prompt LIKE %:prompt%")
     List<CaptchaAnswer> getAllCaptchaAnswersByPrompt(@Param("prompt") String prompt);
 }
