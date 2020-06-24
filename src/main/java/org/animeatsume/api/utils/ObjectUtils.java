3fd8421d58934ff294aa4974cf8e097e1fa57b63
D-Pow <djp460@nyu.edu>
2020-06-24 13:50:45 -0400

    Add new hashResource() function to get the hash of Resource objects
    It was discovered that kissanime's captcha image paths are not unique. In preparation for converting DB to using hashes instead of src paths, add new hashResource() function

--- src/main/java/org/animeatsume/api/utils/ObjectUtils.java
+++ src/main/java/org/animeatsume/api/utils/ObjectUtils.java
@@ -2,7 +2,13 @@ package org.animeatsume.api.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -50,5 +56,24 @@ public class ObjectUtils {
         }
 
         return results;
+    }
+
+    public static String hashResource(Resource resource) {
+        if (resource == null) {
+            return null;
+        }
+
+        try (InputStream imageInputStream = resource.getInputStream()) {
+            byte[] imageBytes = new byte[(int) resource.contentLength()];
+            imageInputStream.read(imageBytes);
+
+            MessageDigest hash = MessageDigest.getInstance("SHA-256");
+            byte[] imageHash = hash.digest(imageBytes);
+            return new BigInteger(1, imageHash).toString(16);
+        } catch (IOException | NoSuchAlgorithmException e) {
+            log.error("Could not hash Resource ({}). Error cause ({}), message = {}", resource, e.getCause(), e.getMessage());
+        }
+
+        return null;
     }
 }
