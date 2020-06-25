23fd855d35a0a64eebd6c80d3ac09f5040c774e9
D-Pow <djp460@nyu.edu>
2020-06-25 14:06:32 -0400

    Add base rendering for Show page, including image and synopsis from Kitsu
    Uses Bootstrap's Card formatting for clean segmentation of content. d-block-inline is needed to force Card divs to reside next to each other

--- client/src/components/Show/Show.js
+++ client/src/components/Show/Show.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { fetchKitsuTitleSearch } from 'services/KitsuAnimeSearchService';
+import Spinner from 'components/ui/Spinner';
 
 function Show(props) {
+    const title = decodeURIComponent(props.title);
+
+    const [ kitsuResult, setKitsuResult ] = useState(null);
+
+    async function fetchKitsuInfo() {
+        const response = await fetchKitsuTitleSearch(title.toLowerCase());
+        const allKitsuResults = response.data;
+        const showInfo = allKitsuResults.find(show => show.attributes.canonicalTitle === title);
+
+        setKitsuResult(showInfo);
+    }
+
+    useEffect(() => {
+        fetchKitsuInfo();
+    }, []);
+
+    const renderBody = () => {
+        if (!kitsuResult) {
+            return (
+                <div className={'full-screen-minus-scrollbar'}>
+                    <Spinner className={'w-25 h-25 absolute-center'} show={true} />
+                </div>
+            );
+        }
+
+        const {
+            canonicalTitle,
+            synopsis,
+            posterImage: {
+                small
+            }
+        } = kitsuResult.attributes;
+
+        return (
+            <div className={'row pt-5'}>
+                <div className={'card'}>
+                    <div className={'card-body'}>
+                        <div className={'col-sm-4 d-inline-block'}>
+                            <img className={'my-auto flex-center'} src={small} alt={canonicalTitle} />
+                        </div>
+
+                        <div className={'col-sm-8 d-inline-block'}>
+                            <div className={'card d-inline-block'}>
+                                <div className={'card-body'}>
+                                    {synopsis}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
     return (
-        <div>{props.title}</div>
+        <React.Fragment>
+            <div className={'row pb-5'}>
+                <h1 className={'text-center mx-auto mt-5'}>
+                    {title}
+                </h1>
+            </div>
+
+            {renderBody()}
+        </React.Fragment>
     );
 }
 
