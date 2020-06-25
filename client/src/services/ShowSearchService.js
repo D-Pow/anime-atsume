b5ba8253a6f904208ff78d0d2772545513283c71
D-Pow <djp460@nyu.edu>
2020-06-25 19:35:05 -0400

    Minor: Use constant instead of magic string in fetch() call

--- client/src/services/ShowSearchService.js
+++ client/src/services/ShowSearchService.js
@@ -1,7 +1,7 @@
 import { SHOW_SEARCH_URL } from 'services/Urls';
 
 export async function searchForShow(title) {
-    return await fetch('/searchKissanime', {
+    return await fetch(SHOW_SEARCH_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
