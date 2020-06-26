e9fecfac0cf68f103c5cc6d2c942b5c758c2d932
D-Pow <djp460@nyu.edu>
2020-06-26 18:14:17 -0400

    Remove unused imports

--- server/src/main/java/org/animeatsume/api/utils/ui4j/PageUtils.java
+++ server/src/main/java/org/animeatsume/api/utils/ui4j/PageUtils.java
@@ -1,13 +1,8 @@
 package org.animeatsume.api.utils.ui4j;
 
 import io.webfolder.ui4j.api.browser.Page;
-import io.webfolder.ui4j.webkit.WebKitMapper;
-import javafx.application.Platform;
-import netscape.javascript.JSObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 public class PageUtils {
     private static final Logger log = LoggerFactory.getLogger(PageUtils.class);
