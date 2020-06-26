dda57cbb8c6a1bcc17a875ff50003cc12664047f
D-Pow <djp460@nyu.edu>
2020-06-25 21:47:16 -0400

    Minor: Replace magic strings with constants

--- client/src/services/Urls.js
+++ client/src/services/Urls.js
@@ -13,8 +13,8 @@ export const EPISODE_HOST_SEARCH_URL = '/getVideosForEpisode';
 export const VIDEO_BASE_PATH = '/video';
 export const IMAGE_BASE_PATH = '/image';
 
-export const getVideoSrcPath = (showName, episodeName, quality, hostUrl) => `/video/${showName}/${episodeName}/${quality}?url=${hostUrl}`;
-export const getImageSrcPath = imageId => `/image/${imageId}`;
+export const getVideoSrcPath = (showName, episodeName, quality, hostUrl) => `${VIDEO_BASE_PATH}/${showName}/${episodeName}/${quality}?url=${hostUrl}`;
+export const getImageSrcPath = imageId => `${IMAGE_BASE_PATH}/${imageId}`;
 
 export function getVideoNameDataFromUrl(kissanimeEpisodeUrl) {
     const [ blank, unused, showName, episodeName ] = new URL(kissanimeEpisodeUrl).pathname.split('/');
