baccee77cc12adfed276c20ba2527cb8160e8f8f
D-Pow <djp460@nyu.edu>
2020-06-29 16:05:31 -0400

    Add util function for executing a CORS request

--- server/src/main/java/org/animeatsume/api/utils/http/CorsProxy.java
+++ server/src/main/java/org/animeatsume/api/utils/http/CorsProxy.java
@@ -2,11 +2,49 @@ package org.animeatsume.api.utils.http;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
 public class CorsProxy {
+    public static ResponseEntity<?> doCorsRequest(HttpMethod method, URI url, Object body, HttpHeaders headers) {
+        String origin = UriParser.getOrigin(url);
+        HttpEntity<Object> corsEntity = getCorsEntity(body, origin, origin, null, headers);
+        List<String> acceptHeaders = corsEntity.getHeaders().get(HttpHeaders.ACCEPT);
+
+        if (acceptHeaders == null || acceptHeaders.size() == 0) {
+            return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body("You must add a value for the 'Accept' header");
+        }
+
+        boolean responseIsText = acceptHeaders.stream().reduce(
+            false,
+            (isText, headerEntry) -> headerEntry.contains("text"),
+            (prevIsText, nextIsText) -> prevIsText || nextIsText
+        );
+
+        Class<?> responseClass = responseIsText ? String.class : Object.class;
+        RestTemplate restTemplate = Requests.getNoFollowRedirectsRestTemplate();
+
+        ResponseEntity<?> response = Requests.doRequestWithStringFallback(restTemplate, url, method, corsEntity, responseClass);
+        Object responseBody = response.getBody();
+        HttpHeaders responseHeaders = Requests.copyHttpHeaders(response.getHeaders());
+
+        // 'Content-Encoding' and 'Transfer-Encoding' mark that the response body is zipped/compressed.
+        // Remove it from the response to the client since it is only relevant for the recipient of the original
+        // content (which is this server).
+        responseHeaders.remove(HttpHeaders.CONTENT_ENCODING);
+        responseHeaders.remove(HttpHeaders.TRANSFER_ENCODING);
+
+        return new ResponseEntity<>(responseBody, responseHeaders, HttpStatus.OK);
+    }
+
     public static <T> HttpEntity<T> getCorsEntity(T body, String origin, String referer) {
         return CorsProxy.getCorsEntity(body, origin, referer, null);
     }
