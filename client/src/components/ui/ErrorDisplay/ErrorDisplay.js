574ec94ed528229370f28feb1ab2d45a84d03de0
D-Pow <djp460@nyu.edu>
2020-07-08 19:04:58 -0400

    Add new ErrorDisplay component for displaying errors to the user

---
+++ client/src/components/ui/ErrorDisplay/ErrorDisplay.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ErrorDisplay(props) {
+    if (!props.show) {
+        return '';
+    }
+
+    return (
+        <div className={`${props.className} ${props.fullScreen ? 'full-screen-minus-scrollbar' : ''}`}>
+            <div className={props.fullScreen ? 'absolute-center' : ''}>
+                <h3 className={'mr-1'}>Sorry something went wrong.</h3>
+                <h3>{props.suggestion}</h3>
+            </div>
+        </div>
+    );
+}
+
+ErrorDisplay.propTypes = {
+    className: PropTypes.string,
+    fullScreen: PropTypes.bool,
+    show: PropTypes.bool,
+    suggestion: PropTypes.string
+};
+
+ErrorDisplay.defaultProps = {
+    className: '',
+    fullScreen: false,
+    show: false,
+    suggestion: 'Try refreshing the page.'
+};
+
+export default ErrorDisplay;
