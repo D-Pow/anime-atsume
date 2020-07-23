b784f252d3510459a68283215bdeacb479029960
D-Pow <djp460@nyu.edu>
2020-07-23 16:59:31 -0400

    Add interval at which to clear Kissanime title-search cache
    To avoid possible issues arising from Kissanime's title/episode searches changing over time and then returning an outdated cached response for `/searchKissanime`, clear the cache every so often (interval is currently weekly).
    
    To do this:
    
    * Enable cron-job-like scheduling within Spring using `@EnableScheduling`.
    * Annotate new `clearKissanimeTitleCache()` function with `@CacheEvict` and `@Scheduled` to clear the cache at a scheduled time
        - `@CacheEvict` specifies which cache (by name) to clear and if all those cache entries should be cleared or not.
        - `@Scheduled` specifies the interval at which to run the `clearKissanimeTitleCache()` function.
    * Log when the cache was cleared.
        - Not necessary, but might as well log this information for easy debugging later.

--- server/src/main/java/org/animeatsume/ApplicationConfig.java
+++ server/src/main/java/org/animeatsume/ApplicationConfig.java
@@ -4,19 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class ApplicationConfig {
     private static final Logger log = LoggerFactory.getLogger(ApplicationConfig.class);
+    private static final long WEEK_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 7;
+    private static final long KISSANIME_TITLE_SEARCH_CACHE_CLEAR_INTERVAL = WEEK_IN_MILLISECONDS;
 
     public static final String KISSANIME_TITLE_SEARCH_CACHE_NAME = "kissanimeTitleSearch";
 
@@ -43,5 +50,11 @@ public class ApplicationConfig {
         log.info("Caching activated for cache names: {}", Arrays.asList(cacheNames));
 
         return new ConcurrentMapCacheManager(cacheNames);
+    }
+
+    @CacheEvict(allEntries = true, value = KISSANIME_TITLE_SEARCH_CACHE_NAME)
+    @Scheduled(fixedDelay = KISSANIME_TITLE_SEARCH_CACHE_CLEAR_INTERVAL)
+    public void clearKissanimeTitleCache() {
+        log.info("Cleared Kissanime-title-search cache at {}", LocalDateTime.now());
     }
 }
