05076d55b1039b7a8608746420c8b4ae5979bd4a
D-Pow <djp460@nyu.edu>
2020-07-23 17:21:04 -0400

    Minor: Remove unused imports

--- server/src/main/java/org/animeatsume/ApplicationDriver.java
+++ server/src/main/java/org/animeatsume/ApplicationDriver.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
