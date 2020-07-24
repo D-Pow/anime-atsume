9b379e36e3166c1f4580cd4a40caad1362ca188b
D-Pow <djp460@nyu.edu>
2020-07-24 10:47:09 -0400

    Add HTML 'title' extractor function to HtmlParser

--- server/src/main/java/org/animeatsume/api/utils/regex/HtmlParser.java
+++ server/src/main/java/org/animeatsume/api/utils/regex/HtmlParser.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class HtmlParser {
     private static final String ANCHOR_URL_REGEX = "(?<=href=\")[^\"]+";
     private static final String ANCHOR_TEXT_REGEX = "(?<=\">)[^<]+";
+    private static final String TITLE_REGEX = "(?<=<title>).+(?=</title>)";
 
     public static String getUrlFromAnchor(String anchor) {
         List<String> firstMatchingGroups = RegexUtils.getFirstMatchGroups(ANCHOR_URL_REGEX, anchor);
@@ -14,6 +15,12 @@ public class HtmlParser {
 
     public static String getTextFromAnchor(String anchor) {
         List<String> firstMatchingGroups = RegexUtils.getFirstMatchGroups(ANCHOR_TEXT_REGEX, anchor);
+
+        return firstMatchingGroups.size() > 0 ? firstMatchingGroups.get(0) : null;
+    }
+
+    public static String getTitleTextFromHtml(String html) {
+        List<String> firstMatchingGroups = RegexUtils.getFirstMatchGroups(TITLE_REGEX, html);
 
         return firstMatchingGroups.size() > 0 ? firstMatchingGroups.get(0) : null;
     }
