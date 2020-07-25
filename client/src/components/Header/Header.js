1187f3361972552cb6fee94a10609e64c71012dd
D-Pow <djp460@nyu.edu>
2020-07-25 19:19:59 -0400

    Heavy update to Header: style updates and accept navEntries as a prop
    This commit applies the same type of styles as seen in [Bootstrap's Navbar docs](https://getbootstrap.com/docs/4.5/components/navbar/) by using `nav` element and .nav(bar|bar-nav|-item|-link) CSS classes. It also uses the `header` element for accessibility purposes.
    
    It also creates a new prop, `navEntries`, that holds an object of customizable props used in the Header nav entries elements, including:
    
    * className props for both `li` wrapper and nested `a` element
    * `href` prop which defaults to null so it won't be added to the underlying `a` element if it isn't specified
    * `onClick` handler function to allow for custom actions upon clicking a nav entry

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,17 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Header(props) {
+    const defaultNavEntryProps = {
+        className: '',
+        wrapperCls: '',
+        href: null,
+        onClick: () => {}
+    };
+
+    const renderedNavEntries = props.navEntries.map((entryProps, i) => {
+        const navEntryProps = { ...defaultNavEntryProps, ...entryProps };
+
+        return (
+            <li className={`nav-item p-2 ${navEntryProps.wrapperCls}`} key={i}>
+                <a
+                    className={`nav-link cursor-pointer ${navEntryProps.className}`}
+                    href={navEntryProps.href}
+                    onClick={navEntryProps.onClick}
+                >
+                    {navEntryProps.children}
+                </a>
+            </li>
+        );
+    });
+
     return (
-        <div>Header</div>
+        <header className={`header ${props.border ? 'header-border' : ''}`}>
+            <nav className={'navbar'}>
+                <ul className={'navbar-nav flex-row justify-content-between'}>
+                    {renderedNavEntries}
+                </ul>
+            </nav>
+        </header>
     );
 }
 
 Header.propTypes = {
-
+    border: PropTypes.bool,
+    navEntries: PropTypes.arrayOf(
+        PropTypes.shape({
+            children: PropTypes.oneOfType([
+                PropTypes.node,
+                PropTypes.arrayOf(PropTypes.node)
+            ]).isRequired,
+            className: PropTypes.string,
+            wrapperCls: PropTypes.string,
+            href: PropTypes.string,
+            onClick: PropTypes.func
+        })
+    )
 };
 
 Header.defaultProps = {
-
+    border: false,
+    navEntries: []
 };
 
 export default Header;
