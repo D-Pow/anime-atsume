e11cb6797eade2f49cf0e374d8a91830d9c95946
D-Pow <djp460@nyu.edu>
2020-07-25 20:19:40 -0400

    Minor: Replace 'p' with 'div' for flexibility with children
    In preparation for adding a new section discussing localStorage usage, which will require a <pre> element that is not a valid descendant of <p>, convert the previous <p> elements to <div> elements for consistency

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -37,18 +37,18 @@ function About(props) {
 
             <div className={'row mt-3'}>
                 <div className={'col-12 col-sm-9 col-md-8 col-lg-6 mx-auto'}>
-                    <p>
+                    <div>
                         We have all been there: you visit some website to watch your
                         favorite show just to be bombarded with ads, weird click-bait,
                         and worst of all, those invisible buttons positioned right
                         over the video that open to a sketchy tab when you try to
                         click the play button.
-                    </p>
-                    <p className={'mt-5'}>
+                    </div>
+                    <div className={'mt-5'}>
                         <span className={'font-weight-bold'}>Anime Atsume</span> is intended to be the exact opposite: overly
                         simplistic, easy to use interface, and the quickest possible
                         path from finding to watching your shows.
-                    </p>
+                    </div>
                 </div>
             </div>
         </div>
