5a28c5c01d0e18d7094fff50e404816a21368720
D-Pow <djp460@nyu.edu>
2020-08-04 19:18:27 -0400

    Enhance IncompatibleBrowserFallback to allow custom text display elements

--- client/src/components/IncompatibleBrowserFallback.js
+++ client/src/components/IncompatibleBrowserFallback.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function IncompatibleBrowserFallback(props) {
+    const DisplayElement = props.displayElement;
     let classNames = '';
 
     if (props.centered) {
@@ -10,9 +11,9 @@ function IncompatibleBrowserFallback(props) {
 
     return (
         <div className={`text-center w-100 ${classNames} ${props.className}`}>
-            <h1 className={'w-80 m-auto'}>
+            <DisplayElement className={'w-80 m-auto'}>
                 {props.text}
-            </h1>
+            </DisplayElement>
         </div>
     );
 }
@@ -20,12 +21,14 @@ function IncompatibleBrowserFallback(props) {
 IncompatibleBrowserFallback.propTypes = {
     className: PropTypes.string,
     centered: PropTypes.bool,
+    displayElement: PropTypes.string,
     text: PropTypes.string
 };
 
 IncompatibleBrowserFallback.defaultProps = {
     className: '',
     centered: true,
+    displayElement: 'h1',
     text: 'Please use a modern browser (Chrome, Firefox) to view this website.'
 };
 
