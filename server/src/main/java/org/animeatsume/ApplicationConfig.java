a0e5662eb7e8f3f66b8569fd7566d61fbfcbd992
D-Pow <djp460@nyu.edu>
2020-09-11 21:26:16 -0400

    Rename config variables to cover all anime, not just Kissanime

--- server/src/main/java/org/animeatsume/ApplicationConfig.java
+++ server/src/main/java/org/animeatsume/ApplicationConfig.java
@@ -25,9 +25,8 @@ import java.util.concurrent.Executor;
 public class ApplicationConfig {
     private static final Logger log = LoggerFactory.getLogger(ApplicationConfig.class);
     private static final long WEEK_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 7;
-    private static final long KISSANIME_TITLE_SEARCH_CACHE_CLEAR_INTERVAL = WEEK_IN_MILLISECONDS;
-
-    public static final String KISSANIME_TITLE_SEARCH_CACHE_NAME = "kissanimeTitleSearch";
+    private static final long ANIME_TITLE_SEARCH_CACHE_CLEAR_INTERVAL = WEEK_IN_MILLISECONDS;
+    public static final String ANIME_TITLE_SEARCH_CACHE_NAME = "animeTitleSearch";
 
     @Bean
     public Executor taskExecutor(
@@ -54,9 +53,9 @@ public class ApplicationConfig {
         return new ConcurrentMapCacheManager(cacheNames);
     }
 
-    @CacheEvict(allEntries = true, value = KISSANIME_TITLE_SEARCH_CACHE_NAME)
-    @Scheduled(fixedDelay = KISSANIME_TITLE_SEARCH_CACHE_CLEAR_INTERVAL)
+    @CacheEvict(allEntries = true, value = ANIME_TITLE_SEARCH_CACHE_NAME)
+    @Scheduled(fixedDelay = ANIME_TITLE_SEARCH_CACHE_CLEAR_INTERVAL)
     public void clearKissanimeTitleCache() {
-        log.info("Cleared Kissanime-title-search cache at {}", LocalDateTime.now());
+        log.info("Cleared anime-title-search cache at {}", LocalDateTime.now());
     }
 }
