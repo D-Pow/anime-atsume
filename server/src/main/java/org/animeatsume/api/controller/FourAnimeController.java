1a417d88e17d95f6a8cdc8c28ad78b498506a8ec
D-Pow <djp460@nyu.edu>
2020-09-11 20:57:29 -0400

    Add async episode-list scraping function for 4anime show URLs

--- server/src/main/java/org/animeatsume/api/controller/FourAnimeController.java
+++ server/src/main/java/org/animeatsume/api/controller/FourAnimeController.java
@@ -4,8 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.animeatsume.api.model.TitleSearchRequest;
 import org.animeatsume.api.model.TitlesEpisodesSearchResults;
 import org.animeatsume.api.service.FourAnimeService;
+import org.animeatsume.api.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -15,6 +20,14 @@ public class FourAnimeController {
 
     public TitlesEpisodesSearchResults searchTitle(TitleSearchRequest request) {
         TitlesEpisodesSearchResults titleResults = fourAnimeService.searchTitle(request.getTitle());
+
+        if (titleResults != null) {
+            List<CompletableFuture<Void>> episodeSearchFutures = titleResults.getResults().stream()
+                .map(titleResult -> fourAnimeService.searchEpisodes(titleResult))
+                .collect(Collectors.toList());
+
+            ObjectUtils.getAllCompletableFutureResults(episodeSearchFutures);
+        }
 
         return titleResults;
     }
--- server/src/main/java/org/animeatsume/api/service/FourAnimeService.java
+++ server/src/main/java/org/animeatsume/api/service/FourAnimeService.java
@@ -3,6 +3,7 @@ package org.animeatsume.api.service;
 import lombok.extern.slf4j.Slf4j;
 import org.animeatsume.api.model.TitlesEpisodesSearchResults;
 import org.animeatsume.api.model.TitlesEpisodesSearchResults.TitleResults;
+import org.animeatsume.api.model.VideoSearchResult;
 import org.animeatsume.api.utils.http.CorsProxy;
 import org.animeatsume.api.utils.http.Requests;
 import org.jsoup.Jsoup;
@@ -13,10 +14,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -25,6 +29,7 @@ public class FourAnimeService {
     private static final String ORIGIN = "https://4anime.to";
     private static final String SEARCH_URL = ORIGIN + "/wp-admin/admin-ajax.php";
     private static final String TITLE_ANCHOR_SELECTOR = "a.name";
+    private static final String EPISODE_ANCHOR_SELECTOR = "ul.episodes a[title]";
 
     @Value("${org.animeatsume.mock-firefox-user-agent}")
     private String mockFirefoxUserAgent;
@@ -70,5 +75,38 @@ public class FourAnimeService {
         }
 
         return null;
+    }
+
+    @Async
+    public CompletableFuture<Void> searchEpisodes(TitleResults titleResults) {
+        log.info("Searching 4anime for episode list at ({}) ...", titleResults.getUrl());
+
+        String showHtml = (String) CorsProxy.doCorsRequest(
+            HttpMethod.GET,
+            URI.create(titleResults.getUrl()),
+            URI.create(ORIGIN),
+            null,
+            getNecessaryRequestHeaders()
+        ).getBody();
+
+        if (showHtml != null) {
+            List<VideoSearchResult> episodeAnchors = Jsoup.parse(showHtml)
+                .select(EPISODE_ANCHOR_SELECTOR)
+                .stream()
+                .map(element -> new VideoSearchResult(
+                    element.attr("href"),
+                    element.text()
+                ))
+                .collect(Collectors.toList());
+
+            titleResults.setEpisodes(episodeAnchors);
+        }
+
+        log.info("Obtained {} episodes for {}",
+            titleResults.getEpisodes().size(),
+            titleResults.getTitle()
+        );
+
+        return CompletableFuture.completedFuture(null);
     }
 }
