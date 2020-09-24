5769a2f476a31554ec404d0b5d1296f699dd4e3e
D-Pow <djp460@nyu.edu>
2020-09-24 15:59:01 -0400

    Enhance corner link component with customizable props

--- client/src/components/ui/GitHubPageCornerLink/GitHubPageCornerLink.js
+++ client/src/components/ui/GitHubPageCornerLink/GitHubPageCornerLink.js
@@ -7,16 +7,19 @@ import { ReactComponent as GitHubLogo } from 'assets/github_logo.svg';
 function GitHubPageCornerLink(props) {
     const isMobile = isMobileBrowser({ onlyXsScreenSizes: true });
     const sizePx = isMobile ? props.sizePx - props.sizePxDecreaseOnMobile : props.sizePx;
+    const linearGradientAngleDegree = 45;
+    const linearGradientAngleFactor = props.side === GitHubPageCornerLink.Sides.LEFT ? -1 : 1;
+    const linearGradientAngle = linearGradientAngleDegree * linearGradientAngleFactor;
 
     return (
         <div
-            className={'text-right float-right position-absolute'}
+            className={`text-${props.side} float-${props.side} position-absolute`}
             style={{
                 top: 0,
-                right: 0,
+                [props.side]: 0,
                 width: `${sizePx}px`,
                 height: `${sizePx}px`,
-                backgroundImage: 'linear-gradient(45deg, transparent 55%, black 45%)',
+                backgroundImage: `linear-gradient(${linearGradientAngle}deg, transparent 55%, ${props.bgColor} 45%)`,
                 zIndex: 1000 // ensure div is placed on top of other, overlapping elements
             }}
         >
@@ -26,7 +29,7 @@ function GitHubPageCornerLink(props) {
                 aria={{
                     style: {
                         top: 6,
-                        right: 6
+                        [props.side]: 6
                     }
                 }}
             >
@@ -36,14 +39,23 @@ function GitHubPageCornerLink(props) {
     );
 }
 
+GitHubPageCornerLink.Sides = {
+    LEFT: 'left',
+    RIGHT: 'right'
+};
+
 GitHubPageCornerLink.propTypes = {
+    bgColor: PropTypes.string,
     href: PropTypes.string,
+    side: PropTypes.oneOf(Object.values(GitHubPageCornerLink.Sides)),
     sizePx: PropTypes.number,
     sizePxDecreaseOnMobile: PropTypes.number
 };
 
 GitHubPageCornerLink.defaultProps = {
+    bgColor: 'black',
     href: '',
+    side: GitHubPageCornerLink.Sides.LEFT,
     sizePx: 100,
     sizePxDecreaseOnMobile: 20
 };
