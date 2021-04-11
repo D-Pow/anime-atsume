627565964cb39afdb90f6f9fa8bdad22e2a6bdfc
D-Pow <djp460@nyu.edu>
2021-04-10 20:20:29 -0600

    Add comment explaining how CSS linear-gradient works
    Will help in understanding what the values mean and why they were set the way they were in the future

--- client/src/components/ui/PageCornerLink/PageCornerLink.js
+++ client/src/components/ui/PageCornerLink/PageCornerLink.js
@@ -32,6 +32,17 @@ function PageCornerLink(props) {
                 [props.side]: 0,
                 width: `${sizePx}px`,
                 height: `${sizePx}px`,
+                /*
+                 * linear-gradient degrees start from 90%, i.e. a vertical line, where Xdeg rotates it clockwise and
+                 * colors go from left to right.
+                 * Color percentages are "distances" from left to right, and mark where the colors stop being 100% their
+                 * color and start transitioning to the next color. If a left color stops before the right color begins,
+                 * they fade into each other (e.g. `30% red, 70% blue` means the red-blue fade goes from 30% to 70%,
+                 * covering 40% of the space); If a left color goes beyond the right color/right color goes under the
+                 * left color, then there is no transition, making a sharp dividing line instead where the left color's
+                 * percentage overrides the right's (e.g. `20% red, 10% blue` means red stops at 20% and blue takes up
+                 * the rest of the space).
+                 */
                 backgroundImage: `linear-gradient(${linearGradientAngle}deg, transparent 55%, ${props.bgColor} 45%)`,
                 zIndex: 1000 // ensure div is placed on top of other, overlapping elements
             }}
