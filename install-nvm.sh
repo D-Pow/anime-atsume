c4df0753c1f79d914dd0f723ba9227c242a9ff7f
D-Pow <D-Pow@users.noreply.github.com>
2022-09-03 02:52:17 -0600

    Add script to install NodeJS (via NVM) before attempting `npm install` in Gradle/Docker
    Also, minor bug fix: Only run front-end install/build commands if its respective build-output dir is empty, not if `node_modules/` is empty (which happens when it's been built by the dev but not copied to the Docker image)

---
+++ install-nvm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S bash
+
+declare _nvmNodeVersion='16'
+
+# Install NVM
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # Load nvm
+[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # Load nvm bash_completion
+export NVM_SYMLINK_CURRENT=true  # Makes a symlink at ~/.nvm/current/bin/node so you don't have to change IDEs' configurations when changing node versions
+export NVM_CURRENT_HOME="$NVM_DIR/current"
+export PATH="$NVM_CURRENT_HOME/bin:$PATH"
+
+# Create `.nvm/current/` dir
+
+declare _nvmCurrentNodePath="$(find -L "$NVM_CURRENT_HOME" -iname node_modules 2>/dev/null \
+    | sed -E 's|.*node_modules/.+||' \
+    | uniq \
+    | sed -E 's/(^\s+)|(\s+$)//g' \
+    | grep -Ev '^[[:space:]]*$'
+)"
+
+export NODE_PATH="${NODE_PATH:+${NODE_PATH}:}$_nvmCurrentNodePath"
+
+nvm install "$_nvmNodeVersion"
+nvm use "$_nvmNodeVersion"
