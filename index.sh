31a44da81f6c87b99981120efd81b4c90302ad72
D-Pow <D-Pow@users.noreply.github.com>
2022-12-01 20:01:07 -0700

    Add new root-level Bash script for repo-wide management

---
+++ index.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env -S bash
+
+declare rootDir="$(realpath -se "$(dirname "${BASH_SOURCE[@]}")")"
+declare clientDir="${rootDir}/client"
+declare serverDir="${rootDir}/server"
+declare buildDir="${serverDir}/build/libs"
+
+
+_egrep() {
+    declare _egrepCommandFlag='-P';
+    declare perlRegexSupported="$(echo 'true' | grep -P 'u' 2>/dev/null)";
+
+    if [[ -z "$perlRegexSupported" ]]; then
+        _egrepCommandFlag='-E';
+    fi;
+
+    grep --exclude-dir={node_modules,.git,.idea,lcov-report} --color=auto $_egrepCommandFlag "$@"
+}
+
+
+clean() (
+    cd "${clientDir}"
+    npm run clean
+
+    cd "${serverDir}"
+    ./gradlew clean
+)
+
+
+build() (
+    cd "${serverDir}"
+
+    ./gradlew build
+)
+
+
+dockerBuild() (
+    docker build -t anime-atsume .
+)
+
+
+deploy() (
+    clean
+    build
+    dockerBuild
+)
+
+
+main() {
+    declare USAGE="${rootDir}/${BASH_SOURCE[0]} [OPTIONS...] <cmd>
+    Run commands for both Anime Atsume's client and server directories.
+
+    Commands:
+$(
+    declare -f \
+        | egrep -i '^[^_][^ \t]+ \(\)' \
+        | egrep -iv 'main' \
+        | sed -E 's/ \(\)//' \
+        | sed -E 's/^/        /'
+)
+    "
+
+    declare OPTIND=1
+
+    while getopts ":h" opt; do
+        case "$opt" in
+            h)
+                echo -e "$USAGE"
+                return 1
+                ;;
+            *)
+                :  # Unknown flag - Forward to desired command
+                ;;
+        esac
+    done
+
+    shift $(( OPTIND - 1 ))
+
+    declare cmd="$1"  # First arg = command to run. Last arg = `${@:$#}``
+    declare cmdArgs="${@:2}"
+
+    $cmd "$cmdArgs"
+}
+
+
+main "$@"
