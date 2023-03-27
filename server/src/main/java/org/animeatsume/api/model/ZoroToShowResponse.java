d149533e3a5d866be5f757616f5d1cacf18e98c7
D-Pow <D-Pow@users.noreply.github.com>
2023-03-27 02:10:50 -0600

    Add new util for parsing `Map` objects from `zoro.to` into response objects

--- server/src/main/java/org/animeatsume/api/model/ZoroToShowResponse.java
+++ server/src/main/java/org/animeatsume/api/model/ZoroToShowResponse.java
@@ -3,7 +3,10 @@ package org.animeatsume.api.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.animeatsume.api.utils.ObjectUtils;
 import org.gradle.internal.impldep.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -14,6 +17,19 @@ public class ZoroToShowResponse {
     private boolean status;
 
     public static ZoroToShowResponse fromString(String str) {
-        return new ZoroToShowResponse(str, true);
+        return ObjectUtils.sanitizeAndParseJsonToClass(str, ZoroToShowResponse.class);
+    }
+
+    public static ZoroToShowResponse fromMap(Map<String, Object> likelyLinkedHashMap) {
+        String html = (String) likelyLinkedHashMap.get("html");
+        boolean status;
+
+        try {
+            status = (Boolean) likelyLinkedHashMap.get("status");
+        } catch (Exception isStringInsteadOfBoolean) {
+            status = Boolean.parseBoolean((String) likelyLinkedHashMap.get("status"));
+        }
+
+        return new ZoroToShowResponse(html, status);
     }
 }
