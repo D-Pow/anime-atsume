877ea9a8e25efb2a5157a4cb2a0781c768331014
D-Pow <D-Pow@users.noreply.github.com>
2023-04-18 23:56:41 -0400

    DRY up client config code so there's one single source of truth for build output path

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -25,7 +25,7 @@ const indexHtmlMetaTagData = {
 // output path for webpack build on machine, not relative paths for index.html
 const relativeBuildOutputPaths = {
     development: '',
-    production: '../server/src/main/resources/public/'
+    production: packageJson.config.buildOutputDir,
 };
 const relativeBuildOutputPath = process.env.NODE_ENV === 'production' ? relativeBuildOutputPaths.production : relativeBuildOutputPaths.development;
 const absoluteBuildOutputPath = path.resolve(paths.root, relativeBuildOutputPath);
